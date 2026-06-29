98b90740cbf67c8e8144a20457c7b4cac03d5bf9
morongmauro <morongmauro@gmail.com>
2026-06-29 10:17:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Reemplaza estos dos valores con los de tu proyecto:
 // Supabase Dashboard → Settings → API → Project URL y "anon" public key
 // =============================================
-window.SUPABASE_URL  = 'https://evabivmzrdclhwlpqdoy.supabase.co/rest/v1/';
+window.SUPABASE_URL  = 'https://evabivmzrdclhwlpqdoy.supabase.co';
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YWJpdm16cmRjbGh3bHBxZG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzY1ODAsImV4cCI6MjA5ODMxMjU4MH0.ZgWQhiCaRyEcwe6W-J_iRtHguPr2ACjCoekdL9cbICo';
